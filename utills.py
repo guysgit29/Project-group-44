380a53e26f9057f11f1bfe93ea6dd8f0de307e57
Nadav <nadave9@gmail.com>
2025-12-23 17:41:21 +0200

    new updates

--- utills.py
+++ utills.py
@@ -1,1 +1,24 @@
+import mysql.connector
+from contextlib import contextmanager
+
+
+@contextmanager
+def db_cur():
+    mydb = None
+    cursor = None
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="theater_db",
+            autocommit=True
+        )
+        cursor = mydb.cursor()
+        yield cursor
+    except mysql.connector.Error as err:
+        raise err
+    finally:
+        if cursor: cursor.close()
+        if mydb: mydb.close()
 #group 44
