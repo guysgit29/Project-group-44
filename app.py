02b6a2bd94ec06e7e38376057bee60623440584d
Nadav <nadave9@gmail.com>
2026-01-04 18:05:43 +0200

    עדכון לבסיס נתונים חדש

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import mysql.connector
 import os
 from contextlib import contextmanager
 from utills import *
-
+#
 app = Flask(__name__)
 # הגדרות Session לעבודה לוקאלית
 session_dir = os.path.join(os.getcwd(), 'flask_session_data')
