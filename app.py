4550c6aacb65d326d9d90732a1e8b30a89a5e199
guyeylat <guy.eylat@gmail.com>
2026-01-05 16:02:46 +0200

    חיפוש טיסה

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home_page():
     origins, destinations = Flight.get_all_origins_and_destinations()
     return render_template('home_page.html', origins=origins, destinations=destinations)
 
-
+#
 @app.route('/search')
 def search_flights():
     """Flight search."""
