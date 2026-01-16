eab755400c9de0d144570cf9041eabb10e9060c6
Admin <zoharlevy23@gmail.com>
2026-01-16 16:28:34 +0200

    דחיפה-דף דוחות מוכן סופית

--- app.py
+++ app.py
@@ -1224,7 +1224,7 @@ def manager_flight_cancel(flight_number):  # Cancels a flight as a manager and r
 
 
 @app.route("/manager_reports", methods=["GET"])
-def manager_reports():  # Displays manager reports dashboard
+def manager_reports():
     if session.get("role") != "manager":
         return redirect(url_for("manager_login"))
 
@@ -1232,8 +1232,16 @@ def manager_reports():  # Displays manager reports dashboard
     r2 = ManagerReports.report_2_revenue_by_aircraft_and_class()
     r3 = ManagerReports.report_3_crew_hours_short_long()
     r4 = ManagerReports.report_4_monthly_cancellation_rate()
+    r5 = ManagerReports.report_5_fleet_monthly_utilization_and_dominant_route()
 
-    return render_template("manager_reports.html", report1_avg=r1, report2_rows=r2, report3_rows=r3, report4_rows=r4)
+    return render_template(
+        "manager_reports.html",
+        report1_avg=r1,
+        report2_rows=r2,
+        report3_rows=r3,
+        report4_rows=r4,
+        report5_rows=r5
+    )
 
 
 @app.route("/profile", methods=["GET", "POST"])
